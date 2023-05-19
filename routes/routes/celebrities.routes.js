7ba240161396b086555b81dd89785eb88ab81829
Daniel Cornejo <contact@danielcornejo.dev>
2023-05-19 16:31:58 -0400

    Iteration 3 Complete

--- routes/routes/celebrities.routes.js
+++ routes/routes/celebrities.routes.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const Celebrity = require("../../models/Celebrity.model");
 const router = express.Router();
-// const Pokemon = require("../models/Pokemon");
-// const Trainer = require("../models/Trainer");
+
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", (req, res) => {
+  res.render('index');
+});
+
+router.get("/celebrities/create", (req, res) => {
+  res.render('celebrities/new-celebrity');
+});
+
+router.post("/celebrities/create", (req, res) => {
+  Celebrity.create({
+    name: req.body.celebName,
+    occupation: req.body.celebOccupation,
+    catchPhrase: req.body.catchPhrase
+  }).then((err, response)=>{
+    if(err) {
+      res.render('celebrities/new-celebrity');
+    }
+    res.redirect('celebrities');
+  })
 });
 
 module.exports = router;
